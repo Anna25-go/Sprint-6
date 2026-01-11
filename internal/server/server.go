7d42946d24098843002a93985ee7f6b2270747ca
Anna25-go <sportani4@gmail.com>
2026-01-11 20:16:46 +0400

    first commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,46 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+type Server struct {
+	logger *log.Logger
+	server *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", handlers.RootHandler)
+	router.HandleFunc("/upload", handlers.UploadHandler)
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	logger.Println("serv good")
+
+	return &Server{
+		logger: logger,
+		server: server,
+	}
+
+}
+
+func (s *Server) Start() {
+	s.logger.Println("Start serv")
+	if err := s.server.ListenAndServe(); err != nil {
+		s.logger.Fatal()
+		return
+	}
+}
